7ce9aa06202f6a20196a7161eff074968b02f493
Afrifa Ebenezer <afrifaeben7@gmail.com>
2026-08-11 17:20:06 -0600

    Add GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
+  site: "https://afrifaeben7.github.io",
+  base: "/tell-church-website",
   output: "static",
   build: {
     format: "directory"
